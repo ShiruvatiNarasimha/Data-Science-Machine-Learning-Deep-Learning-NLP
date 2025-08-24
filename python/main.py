009b6d1bfa4c93045b1c51cfc71d54eca7c1f147
ShiruvatiNarasimha <mrnattyboy05@gmail.com>
2025-08-24 22:51:02 +0530

    Updated main.py

---
+++ python/main.py
@@ -0,0 +1,79 @@
+age = 32
+if age>30:
+    print(age)
+
+total = 1+2+3\
++4+5
+
+
+print(total)
+
+
+x=5; y=10; z=x+y
+print(z)
+
+
+
+
+
+
+age= 28
+if age > 18:
+    print("you are an adult")
+
+
+
+
+
+
+age = 34
+type(age)
+name = "shiruvati"
+type(name)
+
+
+## Declaring And Assigning Variables
+
+age=32
+height=5.9
+name= "shiruvati"
+is_student= True
+
+
+## Printing Variables
+print("age:", age)
+print("height:", height)
+print("name:", name)
+print("is_student:", is_student)
+
+
+heigth=5.1
+type(heigth)
+
+float(int(heigth))
+
+
+
+ ## input
+
+age=input("What is your age?")
+print(age,type(age))
+age=int(input("What is your age?"))
+print(age,type(age))
+
+
+
+## simple calculator
+num1=int(input("Enter first number:"))
+num2=int(input("Enter second number:"))
+
+sum=num1+num2
+difference=num1-num2
+product=num1*num2
+quotient=num1/num2
+
+
+print("Sum:",sum)
+print("Difference:",difference)
+print("Product:",product)
+print("Quotient:",quotient) 
