e9cb5bb9f7ab622eac787f3f89a681048d348b70
ShiruvatiNarasimha <mrnattyboy05@gmail.com>
2025-09-04 21:48:03 +0530

    ## Arithmetic Operators

---
+++ python/operators.py
@@ -0,0 +1,20 @@
+## Arithmetic Operators
+
+
+a = 10
+b = 15
+
+add_result = a + b
+subtract_result = a -b
+multiply_result = a * b
+divide_result = a / b
+modulus_result = a % b
+exponent_result = a ** b
+
+
+print(add_result)
+print(subtract_result)
+print(multiply_result)
+print(divide_result)
+print(modulus_result)
+print(exponent_result)
